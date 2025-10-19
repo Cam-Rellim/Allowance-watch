3aa0368f1c72a1f1703928e30fd30b2d50a25cef
Cam-Rellim <camrellim@tuta.io>
2025-10-19 13:23:51 -0500

    Add files via upload

--- lib/networks.ts
+++ lib/networks.ts
@@ -1,20 +1,20 @@
-import { createPublicClient, http } from "viem";
-import { CHAIN_BY_ID } from "../config/chains";
+import { createPublicClient, http } from 'viem';
+import { CHAIN_BY_ID } from '../config/chains';
 
 function rpcFor(chainId: number): string {
   switch (chainId) {
     case 1:
       return process.env.NEXT_PUBLIC_RPC_MAINNET
         ?? process.env.NEXT_PUBLIC_ETHEREUM_RPC_URL
-        ?? "https://cloudflare-eth.com";
+        ?? 'https://cloudflare-eth.com';
     case 8453:
-      return process.env.NEXT_PUBLIC_RPC_BASE ?? "https://mainnet.base.org";
+      return process.env.NEXT_PUBLIC_RPC_BASE ?? 'https://mainnet.base.org';
     case 42161:
-      return process.env.NEXT_PUBLIC_RPC_ARBITRUM ?? "https://arb1.arbitrum.io/rpc";
+      return process.env.NEXT_PUBLIC_RPC_ARBITRUM ?? 'https://arb1.arbitrum.io/rpc';
     case 56:
-      return process.env.NEXT_PUBLIC_RPC_BSC ?? "https://bsc-dataseed.binance.org";
+      return process.env.NEXT_PUBLIC_RPC_BSC ?? 'https://bsc-dataseed.binance.org';
     case 43114:
-      return process.env.NEXT_PUBLIC_RPC_AVALANCHE ?? "https://api.avax.network/ext/bc/C/rpc";
+      return process.env.NEXT_PUBLIC_RPC_AVALANCHE ?? 'https://api.avax.network/ext/bc/C/rpc';
     default:
       throw new Error(`Unsupported chain ${chainId}`);
   }
