38286bc3fa9983b99561fd7809380cb000ca4d56
Cam-Rellim <camrellim@tuta.io>
2025-10-23 22:11:32 -0500

    Update erc20.ts

--- lib/erc20.ts
+++ lib/erc20.ts
@@ -1,6 +1,20 @@
+// lib/erc20.ts
 export const ERC20_ABI = [
-  { "type": "function", "name": "decimals", "stateMutability": "view", "inputs": [], "outputs": [{"name":"","type":"uint8"}] },
-  { "type": "function", "name": "symbol", "stateMutability": "view", "inputs": [], "outputs": [{"name":"","type":"string"}] },
-  { "type": "function", "name": "allowance", "stateMutability": "view", "inputs": [{"name":"owner","type":"address"},{"name":"spender","type":"address"}], "outputs": [{"name":"","type":"uint256"}] },
-  { "type": "function", "name": "approve", "stateMutability": "nonpayable", "inputs": [{"name":"spender","type":"address"},{"name":"amount","type":"uint256"}], "outputs": [{"name":"","type":"bool"}] }
+  {
+    name: 'allowance',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [
+      { name: 'owner', type: 'address' },
+      { name: 'spender', type: 'address' },
+    ],
+    outputs: [{ name: '', type: 'uint256' }],
+  },
+  {
+    name: 'decimals',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [{ name: '', type: 'uint8' }],
+  },
 ] as const;
